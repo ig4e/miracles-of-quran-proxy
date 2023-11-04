1b4620aaa764206bfaa2b56aedd0dc6726d0b956
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2023-11-04 22:42:22 +0200

    [fix] amp

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,36 @@ app.use(cors());
 app.use(express.json());
 
 app.use(
-	"/",
-	proxy("www.miracles-of-quran.com/", {
+	"/ampproject",
+	proxy("https://cdn.ampproject.org", {
 		memoizeHost: true,
-		https: true,
+		timeout: 60 * 1000,
+	}),
+);
+
+app.use(
+	"/",
+	proxy("www.miracles-of-quran.com", {
+		memoizeHost: true,
+		userResHeaderDecorator: function (headers, req, res) {
+			if (!headers["content-type"]?.includes("text/html")) return headers;
+
+			headers["content-encoding"] = "";
+
+			return headers;
+		},
+
+		userResDecorator: async function (proxyRes, proxyResData, userReq, userRes) {
+			const response = await fetch("https://www.miracles-of-quran.com" + userReq.path);
+
+			if (!response.headers.get("content-type")?.includes("text")) return proxyResData;
+			console.log(response.headers.get("content-type")?.includes("text"));
+
+			const body = await response.text();
+			const filterdBody = body.replaceAll("https://cdn.ampproject.org", "/ampproject");
+
+			return filterdBody;
+		},
 	}),
 );
 
